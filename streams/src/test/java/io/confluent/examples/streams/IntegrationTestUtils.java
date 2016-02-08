7ee614f6bbccc9ddc480e8b10fdb711d6d68f2b0
Michael G. Noll <michael@confluent.io>
2016-02-08 16:36:06 +0100

    Support consuming as many messages as possible (until timeout)

--- streams/src/test/java/io/confluent/examples/streams/IntegrationTestUtils.java
+++ streams/src/test/java/io/confluent/examples/streams/IntegrationTestUtils.java
@@ -14,6 +14,8 @@ import java.util.stream.Collectors;
  */
 public class IntegrationTestUtils {
 
+  private static final int UNLIMITED_MESSAGES = -1;
+
   /**
    * Returns up to `maxMessages` by reading via the provided consumer (the topic(s) to read from are
    * already configured in the consumer).
@@ -24,6 +26,16 @@ public class IntegrationTestUtils {
   public static <K, V> List<V> readValues(KafkaConsumer<K, V> consumer, int maxMessages) {
     List<KeyValue<K, V>> kvs = readKeyValues(consumer, maxMessages);
     return kvs.stream().map(kv -> kv.value).collect(Collectors.toList());
+  }
+
+  /**
+   * Reading as many messages as possible via the provided consumer (the topic(s) to read from are
+   * already configured in the consumer) until a (currently hardcoded) timeout is reached.
+   * @param consumer
+   * @return The KeyValue elements retrieved via the consumer.
+   */
+  public static <K, V> List<KeyValue<K, V>> readKeyValues(KafkaConsumer<K, V> consumer) {
+    return readKeyValues(consumer, UNLIMITED_MESSAGES);
   }
 
   /**
@@ -38,7 +50,7 @@ public class IntegrationTestUtils {
     int maxTotalPollTimeMs = 2000;
     int totalPollTimeMs = 0;
     List<KeyValue<K, V>> consumedValues = new ArrayList<>();
-    while (totalPollTimeMs < maxTotalPollTimeMs && consumedValues.size() < maxMessages) {
+    while (totalPollTimeMs < maxTotalPollTimeMs && continueConsuming(consumedValues.size(), maxMessages)) {
       totalPollTimeMs += pollIntervalMs;
       ConsumerRecords<K, V> records = consumer.poll(pollIntervalMs);
       for (ConsumerRecord<K, V> record : records) {
@@ -46,6 +58,14 @@ public class IntegrationTestUtils {
       }
     }
     return consumedValues;
+  }
+
+  private static boolean continueConsuming(int messagesConsumed, int maxMessages) {
+    if (maxMessages <= 0) {
+      return true;
+    } else {
+      return messagesConsumed < maxMessages;
+    }
   }
 
 }
