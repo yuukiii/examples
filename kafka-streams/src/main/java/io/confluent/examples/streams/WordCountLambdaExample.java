ce9c1fd78e9f2faf5af104d78ae1517a349b247b
Eno Thereska <eno@confluent.io>
2016-05-10 15:35:45 +0100

    Added auto reset config param

--- kafka-streams/src/main/java/io/confluent/examples/streams/WordCountLambdaExample.java
+++ kafka-streams/src/main/java/io/confluent/examples/streams/WordCountLambdaExample.java
@@ -15,6 +15,7 @@
  */
 package io.confluent.examples.streams;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
@@ -56,6 +57,7 @@ public class WordCountLambdaExample {
         // Specify default (de)serializers for record keys and for record values.
         streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         // Set up serializers and deserializers, which we will use for overriding the default serdes
         // specified above.
