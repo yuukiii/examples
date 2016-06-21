de46fa336cca8a70c488bc972aaff5587c370dfc
Michael G. Noll <michael@confluent.io>
2016-06-21 16:27:06 +0200

    Simplify and fix user region example (no Avro anymore)

--- kafka-streams/src/main/java/io/confluent/examples/streams/UserRegionLambdaExample.java
+++ kafka-streams/src/main/java/io/confluent/examples/streams/UserRegionLambdaExample.java
@@ -13,19 +13,16 @@
  */
 package io.confluent.examples.streams;
 
-import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
 import org.apache.kafka.streams.kstream.KTable;
 
 import java.util.Properties;
-
-import io.confluent.examples.streams.utils.GenericAvroSerde;
-import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
 
 /**
  * Computes, per region, the number of users with "complete" user profiles for such regions that
@@ -33,6 +30,89 @@ import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
  * "complete" whenever it has a total of at least 200 characters.
  *
  * Note: This example uses lambda expressions and thus works with Java 8+ only.
+ *
+ * HOW TO RUN THIS EXAMPLE
+ *
+ * 1) Start Zookeeper and Kafka. Please refer to <a href='http://docs.confluent.io/3.0.0/quickstart.html#quickstart'>CP3.0.0
+ * QuickStart</a>.
+ *
+ * 2) Create the input and output topics used by this example.
+ *
+ * <pre>
+ * {@code
+ * $ bin/kafka-topics --create --topic UserRegions \
+ *                    --zookeeper localhost:2181 --partitions 1 --replication-factor 1
+ * $ bin/kafka-topics --create --topic LargeRegions \
+ *                    --zookeeper localhost:2181 --partitions 1 --replication-factor 1
+ * }
+ * </pre>
+ *
+ * Note: The above commands are for CP 3.0.0 only. For Apache Kafka it should be
+ * `bin/kafka-topics.sh ...`.
+ *
+ * 3) Start this example application either in your IDE or on the command line.
+ *
+ * If via the command line please refer to <a href='https://github.com/confluentinc/examples/tree/master/kafka-streams#packaging-and-running'>Packaging</a>.
+ * Once packaged you can then run:
+ *
+ * <pre>
+ * {@code
+ * $ java -cp target/streams-examples-3.0.0-standalone.jar io.confluent.examples.streams.UserRegionLambdaExample
+ * }
+ * </pre>
+ *
+ * 4) Write some input data to the source topics (e.g. via `kafka-console-producer`.  The already
+ * running example application (step 3) will automatically process this input data and write the
+ * results to the output topic.
+ *
+ * <pre>
+ * {@code
+ * # Start the console producer, then input some example data records.  The input data you enter
+ * # should be in the form of USER,REGION<ENTER> and, because this example is set to discard any
+ * # regions that have a user count of only 1, at least one region should have two users or more --
+ * # otherwise this example won't produce any output data (cf. step 5).
+ * #
+ * # alice,asia<ENTER>
+ * # bob,americas<ENTER>
+ * # chao,asia<ENTER>
+ * # dave,europe<ENTER>
+ * # alice,europe<ENTER>        <<< Note: Alice moved from Asia to Europe
+ * # eve,americas<ENTER>
+ * # fang,asia<ENTER>
+ * # gandalf,europe<ENTER>
+ * #
+ * # Here, the part before the comma will become the message key, and the part after the comma will
+ * # become the message value.
+ * $ bin/kafka-console-producer --broker-list localhost:9092 --topic UserRegions \
+ *                              --property parse.key=true --property key.separator=,
+ * }
+ * </pre>
+ *
+ * 5) Inspect the resulting data in the output topics, e.g. via `kafka-console-consumer`.
+ *
+ * <pre>
+ * {@code
+ * $ bin/kafka-console-consumer --topic LargeRegions --from-beginning \
+ *        --zookeeper localhost:2181 \
+ *        --property print.key=true \
+ *        --property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer
+ * }
+ * </pre>
+ *
+ * You should see output data similar to:
+ *
+ * <pre>
+ * {@code
+ * asia     2     # because Alice and Chao are currently in Asia
+ * europe   2     # because Dave and Alice (who moved from Asia to Europe) are currently in Europe
+ * americas 2     # because Bob and Eve are currently in Americas
+ * asia     2     # because Chao and Fang are currently in Asia
+ * europe   3     # because Dave, Alice, and Gandalf are currently in Europe
+ * }
+ * </pre>
+ *
+ * 6) Once you're done with your experiments, you can stop this example via `Ctrl-C`.  If needed,
+ * also stop the Kafka broker (`Ctrl-C`), and only then stop the ZooKeeper instance (`Ctrl-C`).
  */
 public class UserRegionLambdaExample {
 
@@ -45,11 +125,9 @@ public class UserRegionLambdaExample {
     streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
     // Where to find the corresponding ZooKeeper ensemble.
     streamsConfiguration.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, "localhost:2181");
-    // Where to find the Confluent schema registry instance(s)
-    streamsConfiguration.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
     // Specify default (de)serializers for record keys and for record values.
     streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-    streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, GenericAvroSerde.class);
+    streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
 
     final Serde<String> stringSerde = Serdes.String();
     final Serde<Long> longSerde = Serdes.Long();
@@ -57,21 +135,29 @@ public class UserRegionLambdaExample {
     KStreamBuilder builder = new KStreamBuilder();
 
     // Read the source stream
-    KTable<String, GenericRecord> userProfiles = builder.table("UserProfiles");
+    // We assume record key = username and record value = geo-region
+    KTable<String, String> userRegions = builder.table("UserRegions");
 
     // Aggregate the user counts of by region
-    KTable<String, Long> regionCount = userProfiles
-        // Filter out incomplete profiles with less than 200 characters
-        .filter((userId, record) -> ((String) record.get("experience")).getBytes().length > 200)
+    KTable<String, Long> regionCounts = userRegions
         // Count by region
         // We do not need to specify any explict serdes because the key and value types do not change
-        .groupBy((userId, record) -> KeyValue.pair((String) record.get("region"), record))
-        .count("CountsByRegion)")
-        // filter out regions with less than 10M users
-        .filter((regionName, count) -> count > 10 * 1000 * 1000);
+        .groupBy((userId, region) -> KeyValue.pair(region, region))
+        .count("CountsByRegion")
+        // discard any regions with only 1 user
+        .filter((regionName, count) -> count >= 2);
+
+    // Note: The following operations would NOT be needed for the actual users-per-region
+    // computation, which would normally stop at the filter() above.  We use the operations
+    // below only to "massage" the output data so it is easier to inspect on the console via
+    // kafka-console-consumer.
+    //
+    KStream<String, Long> regionCountsForConsole = regionCounts
+        .toStream()
+        .filter((regionName, count) -> count != null);
 
     // write to the result topic, we need to override the value serializer to for type long
-    regionCount.to(stringSerde, longSerde, "LargeCountsByRegion");
+    regionCountsForConsole.to(stringSerde, longSerde, "LargeRegions");
 
     KafkaStreams streams = new KafkaStreams(builder, streamsConfiguration);
     streams.start();
