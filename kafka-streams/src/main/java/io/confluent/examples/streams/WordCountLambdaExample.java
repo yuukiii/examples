f3ecd1ca1d9d9a82fa58cc81ddd53b291780953e
Michael G. Noll <michael@confluent.io>
2016-07-08 15:56:47 +0200

    Add Scala example for WordCount, make WordCount + Scala examples consistent

--- kafka-streams/src/main/java/io/confluent/examples/streams/WordCountLambdaExample.java
+++ kafka-streams/src/main/java/io/confluent/examples/streams/WordCountLambdaExample.java
@@ -1,17 +1,15 @@
 /**
  * Copyright 2016 Confluent Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package io.confluent.examples.streams;
 
@@ -33,13 +31,12 @@ import java.util.Properties;
  *
  * In this example, the input stream reads from a topic named "TextLinesTopic", where the values of
  * messages represent lines of text; and the histogram output is written to topic
- * "WordsWithCountsTopic", where each record is an updated count of a single word, i.e.
- * `word (String) -> currentCount (Long)`.
+ * "WordsWithCountsTopic", where each record is an updated count of a single word, i.e. `word
+ * (String) -> currentCount (Long)`.
  *
- * Note: Before running this example you must 1) create the source topic (e.g. via
- * `kafka-topics --create ...`), then 2) start this example and 3) write some data to
- * the source topic (e.g. via `kafka-console-producer`). Otherwise you won't see any data
- * arriving in the output topic.
+ * Note: Before running this example you must 1) create the source topic (e.g. via `kafka-topics
+ * --create ...`), then 2) start this example and 3) write some data to the source topic (e.g. via
+ * `kafka-console-producer`). Otherwise you won't see any data arriving in the output topic.
  *
  *
  * HOW TO RUN THIS EXAMPLE
@@ -83,6 +80,7 @@ import java.util.Properties;
  * #
  * #   hello kafka streams<ENTER>
  * #   all streams lead to kafka<ENTER>
+ * #   join kafka summit<ENTER>
  * #
  * # Every line you enter will become the value of a single Kafka message.
  * $ bin/kafka-console-producer --broker-list localhost:9092 --topic TextLinesTopic
@@ -111,6 +109,9 @@ import java.util.Properties;
  * lead     1
  * to       1
  * kafka    2
+ * join     1
+ * kafka    3
+ * summit   1
  * }
  * </pre>
  *
@@ -119,63 +120,63 @@ import java.util.Properties;
  */
 public class WordCountLambdaExample {
 
-    public static void main(String[] args) throws Exception {
-        Properties streamsConfiguration = new Properties();
-        // Give the Streams application a unique name.  The name must be unique in the Kafka cluster
-        // against which the application is run.
-        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-lambda-example");
-        // Where to find Kafka broker(s).
-        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        // Where to find the corresponding ZooKeeper ensemble.
-        streamsConfiguration.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, "localhost:2181");
-        // Specify default (de)serializers for record keys and for record values.
-        streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+  public static void main(String[] args) throws Exception {
+    Properties streamsConfiguration = new Properties();
+    // Give the Streams application a unique name.  The name must be unique in the Kafka cluster
+    // against which the application is run.
+    streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-lambda-example");
+    // Where to find Kafka broker(s).
+    streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    // Where to find the corresponding ZooKeeper ensemble.
+    streamsConfiguration.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, "localhost:2181");
+    // Specify default (de)serializers for record keys and for record values.
+    streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+    streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
 
-        // Set up serializers and deserializers, which we will use for overriding the default serdes
-        // specified above.
-        final Serde<String> stringSerde = Serdes.String();
-        final Serde<Long> longSerde = Serdes.Long();
+    // Set up serializers and deserializers, which we will use for overriding the default serdes
+    // specified above.
+    final Serde<String> stringSerde = Serdes.String();
+    final Serde<Long> longSerde = Serdes.Long();
 
-        // In the subsequent lines we define the processing topology of the Streams application.
-        KStreamBuilder builder = new KStreamBuilder();
+    // In the subsequent lines we define the processing topology of the Streams application.
+    KStreamBuilder builder = new KStreamBuilder();
 
-        // Construct a `KStream` from the input topic "TextLinesTopic", where message values
-        // represent lines of text (for the sake of this example, we ignore whatever may be stored
-        // in the message keys).
+    // Construct a `KStream` from the input topic "TextLinesTopic", where message values
+    // represent lines of text (for the sake of this example, we ignore whatever may be stored
+    // in the message keys).
+    //
+    // Note: We could also just call `builder.stream("TextLinesTopic")` if we wanted to leverage
+    // the default serdes specified in the Streams configuration above, because these defaults
+    // match what's in the actual topic.  However we explicitly set the deserializers in the
+    // call to `stream()` below in order to show how that's done, too.
+    KStream<String, String> textLines = builder.stream(stringSerde, stringSerde, "TextLinesTopic");
+
+    KStream<String, Long> wordCounts = textLines
+        // Split each text line, by whitespace, into words.  The text lines are the record
+        // values, i.e. we can ignore whatever data is in the record keys and thus invoke
+        // `flatMapValues` instead of the more generic `flatMap`.
+        .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
+        // We will subsequently invoke `countByKey` to count the occurrences of words, so we use
+        // `map` to ensure the key of each record contains the respective word.
+        .map((key, word) -> new KeyValue<>(word, word))
+        // Count the occurrences of each word (record key).
         //
-        // Note: We could also just call `builder.stream("TextLinesTopic")` if we wanted to leverage
-        // the default serdes specified in the Streams configuration above, because these defaults
-        // match what's in the actual topic.  However we explicitly set the deserializers in the
-        // call to `stream()` below in order to show how that's done, too.
-        KStream<String, String> textLines = builder.stream(stringSerde, stringSerde, "TextLinesTopic");
+        // This will change the stream type from `KStream<String, String>` to
+        // `KTable<String, Long>` (word -> count).  We must provide a name for
+        // the resulting KTable, which will be used to name e.g. its associated
+        // state store and changelog topic.
+        .countByKey("Counts")
+        // Convert the `KTable<String, Long>` into a `KStream<String, Long>`.
+        .toStream();
 
-        KStream<String, Long> wordCounts = textLines
-            // Split each text line, by whitespace, into words.  The text lines are the record
-            // values, i.e. we can ignore whatever data is in the record keys and thus invoke
-            // `flatMapValues` instead of the more generic `flatMap`.
-            .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
-            // We will subsequently invoke `countByKey` to count the occurrences of words, so we use
-            // `map` to ensure the key of each record contains the respective word.
-            .map((key, word) -> new KeyValue<>(word, word))
-            // Count the occurrences of each word (record key).
-            //
-            // This will change the stream type from `KStream<String, String>` to
-            // `KTable<String, Long>` (word -> count).  We must provide a name for
-            // the resulting KTable, which will be used to name e.g. its associated
-            // state store and changelog topic.
-            .countByKey("Counts")
-            // Convert the `KTable<String, Long>` into a `KStream<String, Long>`.
-            .toStream();
+    // Write the `KStream<String, Long>` to the output topic.
+    wordCounts.to(stringSerde, longSerde, "WordsWithCountsTopic");
 
-        // Write the `KStream<String, Long>` to the output topic.
-        wordCounts.to(stringSerde, longSerde, "WordsWithCountsTopic");
-
-        // Now that we have finished the definition of the processing topology we can actually run
-        // it via `start()`.  The Streams application as a whole can be launched just like any
-        // normal Java application that has a `main()` method.
-        KafkaStreams streams = new KafkaStreams(builder, streamsConfiguration);
-        streams.start();
-    }
+    // Now that we have finished the definition of the processing topology we can actually run
+    // it via `start()`.  The Streams application as a whole can be launched just like any
+    // normal Java application that has a `main()` method.
+    KafkaStreams streams = new KafkaStreams(builder, streamsConfiguration);
+    streams.start();
+  }
 
 }
