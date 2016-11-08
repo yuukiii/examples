ac3ffe90fd93323413d5415a84941f91edd16f9f
Matthias J. Sax <matthias@confluent.io>
2016-11-08 11:56:28 -0800

    removed unnecessary through()

--- kafka-streams/src/main/java/io/confluent/examples/streams/PageViewRegionExample.java
+++ kafka-streams/src/main/java/io/confluent/examples/streams/PageViewRegionExample.java
@@ -134,17 +134,19 @@ public class PageViewRegionExample {
     final KStreamBuilder builder = new KStreamBuilder();
 
     // Create a stream of page view events from the PageViews topic, where the key of
-    // a record is assumed to be the user id (String) and the value an Avro GenericRecord
-    // that represents the full details of the page view event.  See `pageview.avsc` under
+    // a record is assumed to be null and the value an Avro GenericRecord
+    // that represents the full details of the page view event. See `pageview.avsc` under
     // `src/main/avro/` for the corresponding Avro schema.
     final KStream<String, GenericRecord> views = builder.stream("PageViews");
 
+    // Create a keyed stream of page view events from the PageViews stream,
+    // by extracting the user id (String) from the Avro value
     final KStream<String, GenericRecord> viewsByUser = views.map(new KeyValueMapper<String, GenericRecord, KeyValue<String, GenericRecord>>() {
       @Override
       public KeyValue<String, GenericRecord> apply(final String dummy, final GenericRecord record) {
         return new KeyValue<>(record.get("user").toString(), record);
       }
-    }).through("PageViewsByUser");
+    });
 
     // Create a changelog stream for user profiles from the UserProfiles topic,
     // where the key of a record is assumed to be the user id (String) and its value
@@ -152,6 +154,7 @@ public class PageViewRegionExample {
     // corresponding Avro schema.
     final KTable<String, GenericRecord> userProfiles = builder.table("UserProfiles", "UserProfilesStore");
 
+    // Create a changelog stream as a projection of the value to the region attribute only
     final KTable<String, String> userRegions = userProfiles.mapValues(new ValueMapper<GenericRecord, String>() {
       @Override
       public String apply(final GenericRecord record) {
