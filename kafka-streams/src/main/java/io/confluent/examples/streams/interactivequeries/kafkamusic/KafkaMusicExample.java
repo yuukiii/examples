3108e8a1c619dc6b7007be855616473089750130
Michael G. Noll <michael@confluent.io>
2017-03-28 12:28:51 +0200

    Update github references to point to `master` branch

--- kafka-streams/src/main/java/io/confluent/examples/streams/interactivequeries/kafkamusic/KafkaMusicExample.java
+++ kafka-streams/src/main/java/io/confluent/examples/streams/interactivequeries/kafkamusic/KafkaMusicExample.java
@@ -98,7 +98,7 @@ import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
  * 3) Start two instances of this example application either in your IDE or on the command
  * line.
  *
- * If via the command line please refer to <a href='https://github.com/confluentinc/examples/tree/3.2.x/kafka-streams#packaging-and-running'>Packaging</a>.
+ * If via the command line please refer to <a href='https://github.com/confluentinc/examples/tree/master/kafka-streams#packaging-and-running'>Packaging</a>.
  *
  * Once packaged you can then start the first instance of the application (on port 7070):
  *
