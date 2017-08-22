c8586b292cde7a12b118f5caeb290d39908a5082
Alex Ayars <alex.ayars@confluent.io>
2017-08-21 19:36:08 -0700

    Extract kafka-streams examples into kafka-streams-examples repo

--- kafka-streams/src/test/java/io/confluent/examples/streams/JoinLambdaIntegrationTest.java
+++
@@ -1,7 +0,0 @@
-package io.confluent.examples.streams;
-
-/**
- * JoinLambdaIntegrationTest was renamed to StreamToTableJoinIntegrationTest.
- */
-public class JoinLambdaIntegrationTest {
-}
--- kafka-streams/src/test/scala/io/confluent/examples/streams/JoinScalaIntegrationTest.scala
+++
@@ -1,7 +0,0 @@
-package io.confluent.examples.streams
-
-/**
-  * JoinScalaIntegrationTest was renamed to StreamToTableJoinScalaIntegrationTest.
-  */
-class JoinScalaIntegrationTest {
-}
